2b21a6ffe72f6bf6476b0d90ebe3dacd39b5d8af
Александр <sayz123456@mail.ru>
2025-02-08 15:49:39 +0400

    Adding a loader to load the main video

--- frontend/src/view/HomePage.vue
+++ frontend/src/view/HomePage.vue
@@ -1,23 +1,24 @@
-
 <template>
-  <Header />
+  <div v-if="isLoading" class="loader">
+    <div class="logo">
+      <span class="bracket">[</span> Sc.AI <span class="bracket">]</span>
+    </div>
+  </div>
 
-  <HeroSectionComponent class="section-spacing" />
-
-  <FeaturesComponent class="section-spacing" />
-
-  <HowItWorksComponent class="section-spacing1" />
-
-  <WhyChooseUsComponent />
-
-  <ApplicationDomainsComponent class="section-spacing4"/>
-
-  <ContactFormComponent class="section-spacing5" />
-
-  <FooterComponent />
+  <div v-show="!isLoading">
+    <Header />
+    <HeroSectionComponent @videoLoaded="hideLoader" class="section-spacing" />
+    <FeaturesComponent class="section-spacing" />
+    <HowItWorksComponent class="section-spacing1" />
+    <WhyChooseUsComponent />
+    <ApplicationDomainsComponent class="section-spacing4"/>
+    <ContactFormComponent class="section-spacing5" />
+    <FooterComponent />
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Header from '@/components/HeaderComponent.vue'
 import HeroSectionComponent from "@/components/HeroSectionComponent.vue";
 import FeaturesComponent from "@/components/FeaturesComponent.vue";
@@ -27,7 +28,13 @@ import ApplicationDomainsComponent from "@/components/ApplicationDomainsComponen
 import ContactFormComponent from "@/components/ContactFormComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 
+const isLoading = ref(true);
 
+const hideLoader = () => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 500);
+};
 </script>
 
 
@@ -69,6 +76,44 @@ body {
   line-height: 1.6;
   background-color: #070514;
   z-index: -3;
+}
+/* Лоадер */
+.loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #070514;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+  transition: opacity 0.8s ease-in-out;
+}
+
+.loader .logo {
+  font-size: 48px;
+  font-weight: 600;
+  color: #444;
+  transition: color 2s ease-in-out;
+  animation: fadeColor 2s infinite alternate;
+}
+
+.loader .bracket {
+  color: #444;
+  transition: color 2s ease-in-out;
+  animation: fadeBracket 2s infinite alternate;
+}
+
+@keyframes fadeColor {
+  0% { color: #444; }
+  100% { color: #00BBFF; }
+}
+
+@keyframes fadeBracket {
+  0% { color: #444; }
+  100% { color: #00BBFF; }
 }
 
 </style>
