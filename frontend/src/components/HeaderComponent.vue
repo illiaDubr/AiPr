72aaf1f237320c5b026c8915e7132952c7f775a8
Александр <sayz123456@mail.ru>
2025-02-11 16:20:17 +0400

    change button

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -40,7 +40,7 @@
         </li>
           <!-- Кнопка будет видна только на экранах меньше 480px -->
           <a href="#contact" class="consortium-btn mobile-btn">
-            Join the Consortium
+            Join
             <span aria-hidden="true" class="arrow">
               <span aria-hidden="true" class="arrow"><svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M5.93167 11.9996L18.2773 11.9996" stroke="#0F1117" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
